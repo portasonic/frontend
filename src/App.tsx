eeb39ea76452bdeaaedf8dde28964d6c0e13cd2c
overflowerror <mail@overfloweror.com>
2021-07-29 21:53:13 +0200

    added structure for auth stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import {AuthenticationProvider} from "./auth";
+import React from "react";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import PrivateRoute from "./auth/PrivateRoute";
 
 function App() {
+
+  console.log("hello world")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <AuthenticationProvider>
+          <BrowserRouter>
+              <Routes>
+                  <Route path={"/login"}>
+                      <h1>Login</h1>
+                  </Route>
+                  <PrivateRoute path={"/"} roles={["user"]}>
+                      <h1>Private</h1>
+                  </PrivateRoute>
+                  <Route path={"/*"}>
+                      <h1>Test</h1>
+                  </Route>
+              </Routes>
+          </BrowserRouter>
+      </AuthenticationProvider>
   );
 }
 
